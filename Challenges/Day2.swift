51503a2fd47105a6a2d2ba4f2ae8de7f360b211a
0bmxa <0bmxa@users.noreply.github.com>
2021-12-02 11:00:37 +0100

    Performance++

--- Challenges/Day2.swift
+++ Challenges/Day2.swift
@@ -7,32 +7,37 @@
 
 import Foundation
 
+struct BoatVector {
+    var x: Int32
+    var y: Int32
+}
+
 struct Day2: Challenge {
-    private let input: [CGVector]
+    private let input: [BoatVector]
 
     init() {
         let file = AOC.getInput(from: "Day2.txt")
         self.input = file.split(whereSeparator: \.isNewline).map { line in
             let parts = line.split(separator: " ")
             let direction = parts[0]
-            let amount = Int(parts[1])!
+            let amount = Int32(parts[1])!
             
             switch direction {
-            case "forward": return CGVector(dx: amount, dy: 0)
-            case "down":    return CGVector(dx: 0, dy: amount)
-            case "up":      return CGVector(dx: 0, dy: -amount)
+            case "forward": return BoatVector(x: amount, y: 0)
+            case "down":    return BoatVector(x: 0, y: amount)
+            case "up":      return BoatVector(x: 0, y: amount * -1)
             default: fatalError()
             }
         }
     }
 
     func runPuzzle1() {
-        let sum = self.input.reduce(into: CGVector()) { sum, current in
-            sum.dx += current.dx
-            sum.dy += current.dy
+        let sum = self.input.reduce(into: BoatVector(x: 0, y: 0)) { sum, current in
+            sum.x += current.x
+            sum.y += current.y
         }
 
-        let result = sum.dx * sum.dy
+        let result = sum.x * sum.y
         
         PrintBuffer.add("\(result)")
     }
